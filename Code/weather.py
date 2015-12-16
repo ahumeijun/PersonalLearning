1e229cf5f720dcb708fc6b24e7d4cad3d1badb9d
Mei Jun <ahumeijun@live.com>
2015-12-16 22:33:16 +0800

    http weather api, rename string.py

--- Code/weather.py
+++ Code/weather.py
@@ -1,16 +1,19 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, urllib, urllib2, json
+import sys, json
+import urllib.request
+import urllib.response
+import urllib.error
 
-url = 'http://apis.baidu.com/heweather/weather/free?city=beijing'
+url = 'http://apis.baidu.com/heweather/weather/free?city=London'
 appkey = '75d9cb08b74346ac08d21e5e55217904'
 
-req = urllib2.Request(url)
+req = urllib.request.Request(url)
 
 req.add_header("apikey", appkey)
 
-resp = urllib2.urlopen(req)
+resp = urllib.request.urlopen(req)
 content = resp.read()
 if(content):
     print(content)
